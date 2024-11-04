a91b6a478eee47366859a8e57f921206fbb7b381
JuanIJX <srwe-@hotmail.com>
2024-11-04 21:44:08 +0100

    Code compress

--- index.js
+++ index.js
@@ -24,20 +24,16 @@ export const createDirs = src => {
 		}
 	}
 	return ret;
-}
-export const readable = src => { try { fs.accessSync(src, constants.R_OK); return true; } catch (error) { return false; } }
-export const writable = src => { try { fs.accessSync(src, constants.R_OK | constants.W_OK); return true; } catch (error) { return false; } }
-export const readableAndWritable = src => { try { fs.accessSync(src, constants.R_OK | constants.W_OK); return true; } catch (error) { return false; } }
-export const existsAsync = async src => { try { await fsPromise.access(src); return true; } catch (error) { return false; } }
+};
+export const readable = src => { try { fs.accessSync(src, constants.R_OK); return true; } catch (error) { return false; } };
+export const writable = src => { try { fs.accessSync(src, constants.R_OK | constants.W_OK); return true; } catch (error) { return false; } };
+export const readableAndWritable = src => { try { fs.accessSync(src, constants.R_OK | constants.W_OK); return true; } catch (error) { return false; } };
+export const existsAsync = async src => { try { await fsPromise.access(src); return true; } catch (error) { return false; } };
 export const getRelative = (metaurl, ...file) => path.join(path.relative(process.cwd(), path.dirname(fileURLToPath(metaurl))), ...file);
 export const getImportPath = metaurl => path.dirname(fileURLToPath(metaurl));
 export const pathToImport = (metaurl, ruta) => "./" + path.relative(getImportPath(metaurl), ruta).replaceAll("\\", "/");
-export const exec = command => {
-	return new Promise((resolve, reject) => {
-		exec2(command, (error, stdout, stderr) => {
-			if (error)		reject(error);
-			else if(stderr)	reject(stderr);
-			else			resolve(stdout);
-		})
-	});
-}
+export const exec = command => new Promise((resolve, reject) => exec2(command, (error, stdout, stderr) => {
+	if (error)		reject(error);
+	else if(stderr)	reject(stderr);
+	else			resolve(stdout);
+}));
